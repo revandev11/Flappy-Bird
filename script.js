e57c9560cda298592505a8efc8001147e4a6dd40
Revan <abusovrvan34@gmail.com>
2026-05-26 11:49:43 +0400

    feat: collission implemented

--- script.js
+++ script.js
@@ -7,23 +7,68 @@ let birdY = 250;
 const birdX = 50;
 const birdRadius = 15;
 const jump = -5.5;
+let gameOver = false;
+
+let pipes = [];
+const pipeWidth = 60;
+const pipeGap = 140;
+const pipeSpeed = 2;
+let frameCount = 0;
 
 document.addEventListener("keydown", function(e) {
-    if (e.code === "Space") {
-        velocity = jump; 
+    if (e.code === "Space" && !gameOver) {
+        velocity = jump;
     }
 });
 
-function gameLoop() {
-    velocity += gravity;
-    birdY += velocity;
+function spawnPipe() {
+    const minHeight = 50;
+    const maxHeight = canvas.height - pipeGap - minHeight;
+    const topHeight = Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight;
 
-    if (birdY + birdRadius > canvas.height) {
-        birdY = canvas.height - birdRadius;
-        velocity = 0;
+    pipes.push({
+        x: canvas.width,
+        top: topHeight,
+        bottom: canvas.height - topHeight - pipeGap
+    });
+}
+
+function gameLoop() {
+    if (!gameOver) {
+        velocity += gravity;
+        birdY += velocity;
+
+        if (birdY + birdRadius > canvas.height || birdY - birdRadius < 0) {
+            gameOver = true;
+        }
+
+        frameCount++;
+        if (frameCount % 120 === 0) {
+            spawnPipe();
+        }
+
+        for (let i = pipes.length - 1; i >= 0; i--) {
+            pipes[i].x -= pipeSpeed;
+
+            if (birdX + birdRadius > pipes[i].x && birdX - birdRadius < pipes[i].x + pipeWidth) {
+                if (birdY - birdRadius < pipes[i].top || birdY + birdRadius > canvas.height - pipes[i].bottom) {
+                    gameOver = true;
+                }
+            }
+
+            if (pipes[i].x + pipeWidth < 0) {
+                pipes.splice(i, 1);
+            }
+        }
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#2ecc71";
+    for (let i = 0; i < pipes.length; i++) {
+        ctx.fillRect(pipes[i].x, 0, pipeWidth, pipes[i].top);
+        ctx.fillRect(pipes[i].x, canvas.height - pipes[i].bottom, pipeWidth, pipes[i].bottom);
+    }
 
     ctx.beginPath();
     ctx.arc(birdX, birdY, birdRadius, 0, Math.PI * 2);
@@ -31,7 +76,15 @@ function gameLoop() {
     ctx.fill();
     ctx.closePath();
 
-    requestAnimationFrame(gameLoop);
+    if (gameOver) {
+        ctx.fillStyle = "red";
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText("YOU LOSE!", canvas.width / 2, canvas.height / 2);
+    } else {
+        requestAnimationFrame(gameLoop);
+    }
 }
 
+spawnPipe();
 gameLoop();
